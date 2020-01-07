4b5878f38c10522867a24cc262681dc86fa3f2f5
liaoheng <liaohengcn@gmail.com>
2020-01-07 14:53:11 +0800

    更新dict，添加一些词

--- rime.lua
+++ rime.lua
@@ -1,14 +1,15 @@
+--[[
+   https://github.com/hchunhui/librime-lua
+--]]
 function date_translator(input, seg)
-   if (input == "datetime") then
-      --- Candidate(type, start, end, text, comment)
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d %H:%M"), "日期时间"))
-   end
-   if (input == "date") then
-      --- Candidate(type, start, end, text, comment)
+   if input == "datetime" then
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d %H:%M:%S"), "Datetime"))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日 %H时%M分%S秒"), "日期时间"))
+   elseif input == "date" then
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "Date"))
       yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "日期"))
-   end
-   if (input == "time") then
-      --- Candidate(type, start, end, text, comment)
-      yield(Candidate("date", seg.start, seg._end, os.date("%H:%M"), "时间"))
+   elseif input == "time" then
+      yield(Candidate("date", seg.start, seg._end, os.date("%H:%M:%S"), "Time"))
+      yield(Candidate("date", seg.start, seg._end, os.date("%H时%M分%S秒"), "时间"))
    end
 end
